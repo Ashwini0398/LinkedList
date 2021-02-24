c8354459a7bfd4108e1c44fb47ce55a98804c8e2
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-02-24 14:15:56 +0530

    ADDED: created simple linked list

--- src/main/java/list/MyNode.java
+++ src/main/java/list/MyNode.java
@@ -1,4 +1,21 @@
 package list;
 
-public class MyNode {
+public class MyNode <K>{
+
+
+    private  MyNode next;
+    private  K Key;
+    public  MyNode(K key){
+        this.Key=null;
+        this.next=null;
+    }
+
+    public MyNode getNext() {
+        return next;
+    }
+
+    public void setNext(MyNode next) {
+        this.next = next;
+    }
 }
+
